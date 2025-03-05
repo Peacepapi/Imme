7ce02b3f82652240a7ebe2dbf5fdd683aa13fc9d
Peacepapi <khmaipapi@yahoo.com>
2025-03-05 10:22:50 -0500

    connected interface and repo and db

--- Imme/Program.cs
+++ Imme/Program.cs
@@ -1,3 +1,8 @@
+using Imme.Data;
+using Imme.Interfaces;
+using Imme.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IDeckInterface, DeckRepository>();
+builder.Services.AddScoped<ICardInterface, CardRepository>();
+builder.Services.AddControllers().AddNewtonsoftJson(options => {
+    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+});
 
 var app = builder.Build();
 
