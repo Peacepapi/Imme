7255d98ac6030afadc748304a156cf2216a4b435
Peacepapi <khmaipapi@yahoo.com>
2025-03-05 09:54:09 -0500

    Added model and dbcontext

---
+++ Imme/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Imme.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Imme.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+        }
+
+        public DbSet<Deck> Deck { get; set; }
+        public DbSet<Card> Card { get; set; }
+
+    }
+}
---
+++ Imme/Models/Entities/Deck.cs
@@ -0,0 +1,11 @@
+﻿namespace Imme.Models.Entities
+{
+    public class Deck
+    {
+        public int Id { get; set; }
+        public string Topic { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public List<Card> Cards { get; set; } = new List<Card>();
+        
+    }
+}
